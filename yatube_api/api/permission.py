1ed10aa68cc8863e9db614d7ad1e7dd74f1faea8
Глеб Наумов <goobee228@gmail.com>
2023-04-14 17:51:26 +0300

    Complete project

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsOwnerCanUpdate(permissions.BasePermission):
+class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method
